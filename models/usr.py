237a32eef04c11f55d74b4c039377a5e3959fe54
stringbitalx <stringbitalx@gmail.com>
2024-03-08 23:18:44 -0800

    usr update

--- models/usr.py
+++ models/usr.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
-from models.base_model import BMdl
+from models.base_mdl import BaseMdl
 
 
-class Usr(BMdl):
+class Usr(BaseMdl):
     """Creates a new user"""
     email = ""
     password = ""
